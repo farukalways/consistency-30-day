e2bd9b5af92cb8b0d565ae85a525bcf930d0108a
farukalways <alishopon367@gmail.com>
2026-08-01 21:44:17 +0600

    day 01 - problem-01 solve

--- day-01/problem_01.js
+++ day-01/problem_01.js
@@ -1,1 +1,28 @@
-// problem 01
+// দুইটি সংখ্যার মধ্যে বড়টি বের করো।
+// system 1
+function bigNumber(a, b) {
+  if (!Number.isNaN(a) || !Number.isNaN(b)) {
+    if (a < b) {
+      return b;
+    } else if (b < a) {
+      return a;
+    }
+  } else {
+    return "invalid number";
+  }
+}
+
+// system 2
+function bigNumber2(a, b) {
+  if (
+    typeof a !== "number" ||
+    typeof b !== "number" ||
+    Number.isNaN(a) ||
+    Number.isNaN(b)
+  ) {
+    return "invalid number";
+  }
+  return Math.max(a, b);
+}
+
+console.log(bigNumber2(55, 4));
