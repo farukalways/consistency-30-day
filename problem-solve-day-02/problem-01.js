26d2753ca36ec2e1271a8658a01086d45c58b003
farukalways <alishopon367@gmail.com>
2026-08-02 06:12:10 +0600

    3 basic problem solve

---
+++ problem-solve-day-02/problem-01.js
@@ -0,0 +1,36 @@
+// problem 1
+// ১ থেকে ১০ পর্যন্ত সংখ্যা প্রিন্ট করো।
+
+// for (let i = 0; i <= 10; i++) {
+//   console.log(i);
+// }
+
+// problem 2
+// ১০ থেকে ১ পর্যন্ত সংখ্যা প্রিন্ট করো।
+
+// for (let i = 10; i >= 1; i--) {
+//   console.log(i);
+// }
+//
+//
+
+// problem 3
+//  ১ থেকে ১০ পর্যন্ত শুধু জোড় সংখ্যা প্রিন্ট করো।
+
+// for (let i = 1; i <= 10; i++) {
+//   if (i % 2 === 0) {
+//     console.log(i);
+//   }
+// }
+
+// problem 3
+// ১ থেকে n পর্যন্ত সংখ্যার যোগফল বের করো।
+
+// const n = 15;
+// let result = 0;
+// for (let i = 0; i < n; i++) {
+//   result += i;
+// }
+
+// anather way
+// const result2 = (n * (n + 1)) / 2;
