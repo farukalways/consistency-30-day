5925c6e51c05cfca93eade5ad718157802dc23f2
farukalways <alishopon367@gmail.com>
2026-08-04 20:26:47 +0600

    solve 2 poblem

---
+++ problem-solve-day-04/problem-02.js
@@ -0,0 +1,13 @@
+// একটি অ্যারের সব স্ট্রিং কে বড় হাতের অক্ষরে রূপান্তর করো।
+
+const fruits = ["apple", "banana", "cherry", "mango", "orange"];
+
+function uppercaseString(arr) {
+  for (let i = 0; i < arr.length; i++) {
+    arr[i] = arr[i].toUpperCase();
+  }
+  return arr.length;
+}
+
+console.log(uppercaseString(fruits));
+console.log(fruits);
