b763d8fc836081d1d2f471c2e7293b7776fb7d99
farukalways <alishopon367@gmail.com>
2026-08-20 06:51:35 +0600

    solve 1 problem today

---
+++ problem-solve-day-19/p-1.js
@@ -0,0 +1,12 @@
+"use strict";
+function getTicketPrice(age) {
+  if (age < 5) {
+    return 0;
+  } else if (age >= 5 && age <= 12) {
+    return 100;
+  } else if (age >= 13 && age <= 59) {
+    return 200;
+  }
+  return 120;
+}
+console.log(getTicketPrice(60));
